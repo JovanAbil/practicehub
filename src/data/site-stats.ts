53131404ffec1de286cc99d08042828f8b6e3416
Jovan <164279478+JovanAbil@users.noreply.github.com>
2026-08-10 11:02:57 -0400

    Update site-stats.ts

--- src/data/site-stats.ts
+++ src/data/site-stats.ts
@@ -18,7 +18,7 @@
  */
 
 export const SITE_STATS = {
-  totalVisitors: 1070,
-  totalPageviews: 10630,
-  lastUpdated: "July 1st, 2026",
+  totalVisitors: 1098,
+  totalPageviews: 11665,
+  lastUpdated: "August 8th, 2026",
 };
