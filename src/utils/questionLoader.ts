9b3b1d9ff1a2090ee090209d5ae70c496392cc9d
Jovan <164279478+JovanAbil@users.noreply.github.com>
2026-09-17 21:48:07 -0400

    Update questionLoader.ts

--- src/utils/questionLoader.ts
+++ src/utils/questionLoader.ts
@@ -46,7 +46,7 @@ import { sequencesseriesQuestions } from '@/data/apcalcbc/sequencesseries-questi
 
 //AP Chem
 import { basicsQuestions } from '@/data/apchem/basics-questions';
-import { atomicQuestions } from '@/data/apchem/atomic-questions';
+import { atomic1Questions } from '@/data/apchem/atomic-questions';
 import { molecular1Questions } from '@/data/apchem/molecular-questions';
 import { intermolecularQuestions } from '@/data/apchem/intermolecular-questions';
 //import { reactionsQuestions } from '@/data/apchem/reactions-questions';
@@ -168,7 +168,7 @@ const questionMap: Record<string, Question[]> = {
 
   //apchem
   'apchem-basics': basicsQuestions,
-  'apchem-atomic': atomicQuestions,
+  'apchem-atomic': atomic1Questions,
   'apchem-molecular': molecular1Questions,
   'apchem-intermolecular': intermolecularQuestions,
   //'apchem-reactions': reactionsQuestions,
@@ -291,7 +291,7 @@ const subjectUnits: Record<string, { id: string; name: string }[]> = {
   apchem: [
       { id: 'basics', name: 'Intro to AP Chem' },
       { id: 'atomic', name: 'Atomic Structure and Properties' },
-      { id: 'molecular1', name: 'Compound Structure and Properties' },
+      { id: 'molecular', name: 'Compound Structure and Properties' },
       { id: 'intermolecular', name: 'Properties and Substances and Mixtures' },
       //{ id: 'reactions', name: 'Chemical Reactions' },
       //{ id: 'kinetics', name: 'Kinetics' },
